b7de010f81b05207c4cbdc01a18a396d47c58f61
Papil10n <nikept.co@gmail.com>
2022-11-15 00:09:42 +0200

    added 3 more tests for the reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,6 +3,7 @@ import {profileAPI, usersAPI} from "../api/api";
 const ADD_POST = "ADD-POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
+const DELETE_POST = "DELETE_POST";
 
 let initialState = {
     posts: [
@@ -17,10 +18,9 @@ let initialState = {
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST: {
-
             return {
                 ...state,
-                posts: [{id: 4, message: action.newPostText, likeCount: 0,}, ...state.posts],
+                posts: [...state.posts, {id: 4, message: action.newPostText, likeCount: 0,}],
             }
 
         }
@@ -29,6 +29,9 @@ const profileReducer = (state = initialState, action) => {
         }
         case SET_STATUS: {
             return {...state, status: action.status};
+        }
+        case DELETE_POST: {
+            return {...state, posts: state.posts.filter(post => post.id != action.postId)}
         }
 
         default:
@@ -40,6 +43,7 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = (newPostText) => ({type: ADD_POST, newPostText});
 export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
 export const setStatus = (status) => ({type: SET_STATUS, status});
+export const deletePost = (postId) => ({type: DELETE_POST, postId});
 
 // thunk
 export const getUserProfile = (userId) => (dispatch) => {
--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,18 +1,54 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import profileReducer, {addPostActionCreator} from "./profile-reducer";
+import profileReducer, {addPostActionCreator, deletePost} from "./profile-reducer";
+
+let state = {
+    posts: [
+        {id: 1, message: 'test проброса пропсов', likeCount: 5},
+        {id: 2, message: 'It\'s my first post', likeCount: 10},
+        {id: 3, message: 'Hi, how are you?', likeCount: 12},
+    ],
+}
 
 it('length of posts should be incremented', () => {
+    // 1. test data
     let action = addPostActionCreator("test add post");
-    let state = {
-        posts: [
-            {id: 1, message: 'test проброса пропсов', likeCount: 5},
-            {id: 2, message: 'It\'s my first post', likeCount: 10},
-            {id: 3, message: 'Hi, how are you?', likeCount: 12},
-        ],
-    }
 
+    // 2 . action
     let newState = profileReducer(state, action);
 
+    //  3. expectation
     expect(newState.posts.length).toBe(4);
+});
+
+it('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPostActionCreator("test add post");
+
+    // 2 . action
+    let newState = profileReducer(state, action);
+
+    //  3. expectation
+    expect(newState.posts[3].message).toBe("test add post");
+});
+
+it('after deleting length of messages should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1);
+
+    // 2 . action
+    let newState = profileReducer(state, action);
+
+    //  3. expectation
+    expect(newState.posts.length).toBe(2);
+});
+
+it('after deleting length shouldn`t be decremented if id is incorrect', () => {
+    // 1. test data
+    let action = deletePost(1000);
+
+    // 2 . action
+    let newState = profileReducer(state, action);
+
+    //  3. expectation
+    expect(newState.posts.length).toBe(3);
 });
