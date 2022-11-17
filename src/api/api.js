490ac6c3871159bc71fcb48fec2b970ded8057b5
Papil10n <nikept.co@gmail.com>
2022-11-17 17:36:53 +0200

    add captcha

--- src/api/api.js
+++ src/api/api.js
@@ -30,8 +30,8 @@ export const authAPI = {
     me() {
         return instance.get('auth/me');
     },
-    login( email, password, rememberMe = false) {
-        return instance.post('auth/login', { email, password, rememberMe });
+    login( email, password, rememberMe = false, captcha = null) {
+        return instance.post('auth/login', { email, password, rememberMe, captcha});
     },
     logout() {
         return instance.delete('auth/login');
@@ -60,5 +60,10 @@ export const profileAPI = {
     saveProfile(profile) {
         return instance.put(`profile`, profile);
     }
+}
 
+export const securityAPI = {
+    getCaptchaUrl() {
+        return instance.get('security/get-captcha-url');
+    }
 }
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,26 +1,28 @@
-import {authAPI} from "../api/api";
+import {authAPI, securityAPI} from "../api/api";
 import {stopSubmit} from "redux-form";
 
 //type of action creator
 const SET_USER_DATA = "network/auth/SET_USER_DATA";
+const GET_CAPTCHA_URL_SUCCESS = "network/auth/GET_CAPTCHA_URL_SUCCESS";
 
 
 let initialState = {
     userId: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: null, // if null,  then captcha is not required
 };
 
 // reducer
 const authReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_USER_DATA:
+        case GET_CAPTCHA_URL_SUCCESS:
             return {
                 ...state,
                 ...action.payload,
             }
-
         default:
             return state;
     }
@@ -31,6 +33,7 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
     type: SET_USER_DATA, payload:
         {userId, email, login, isAuth}
 });
+export const getCaptchaUrlSuccess = (captchaUrl) => ( {type: GET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl}});
 
 // thunk
 export const getAuthUserData = () => async (dispatch) => {
@@ -42,24 +45,31 @@ export const getAuthUserData = () => async (dispatch) => {
     }
 
 }
-
-export const login = (email, password, rememberMe) => async (dispatch) => {
-    let response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe, captcha);
 
     if (response.data.resultCode === 0) {
+        // success, get auth data
         dispatch(getAuthUserData())
     } else {
+        if (response.data.resultCode === 10) {
+            dispatch(getCaptchaUrl());
+        }
         let message = response.data.messages.length > 0 ? response.data.messages[0] : "Some error";
         dispatch(stopSubmit("login", {_error: message}));
     }
 }
-
 export const logout = () => async (dispatch) => {
     let response = await authAPI.logout()
 
     if (response.data.resultCode === 0) {
         dispatch(setAuthUserData(null, null, null, false));
     }
+}
+export const getCaptchaUrl = () => async (dispatch) => {
+    let response = await securityAPI.getCaptchaUrl();
+    const captchaUrl = response.data.url;
+    dispatch(getCaptchaUrlSuccess(captchaUrl));
 }
 
 
